0e62afeee00835f253758ba6620cb8597f7a7f52
Jonathan Chacko <ipp2021043@sicsr.ac.in>
2022-07-13 16:44:42 +0530

    Hidden App Bar

--- app/src/main/java/nox_vision/jcp/vyapari/MainActivity.java
+++ app/src/main/java/nox_vision/jcp/vyapari/MainActivity.java
@@ -3,13 +3,17 @@ package nox_vision.jcp.vyapari;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();//This Line Hides The Action Bar.
         setContentView(R.layout.activity_main);
-        //Checking
     }
 }
